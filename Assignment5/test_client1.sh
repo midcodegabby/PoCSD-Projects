620c89019025c889ba1e6756253c90f63bab757a
midcodegabby <gabrielrodgers4i@gmail.com>
2024-12-02 00:32:51 -0800

    cleaning

--- Assignment5/test_client1.sh
+++
@@ -1,1 +0,0 @@
-python3 fsmain.py -bs 256 -nb 256 -is 32 -ni 32 -cid 1 -port 8000
--- Assignment5/test_server.sh
+++
@@ -1,3 +0,0 @@
-# this file does a test using the server
-# run the server:
-python3 blockserver.py -bs 256 -nb 256 --port 8000 #-delayat 20
